bca330d6f836dbd67d2112035b2aeea20f1edb8d
golfrumors <30129070+golfrumors@users.noreply.github.com>
2023-01-23 19:55:16 -0500

    create basic network headers

--- src/include/network/udp.hpp
+++ src/include/network/udp.hpp
@@ -1,1 +1,18 @@
-///header for the UDP protocol
+//header for the UDP protocol
+#ifndef UDP_HPP
+#define UDP_HPP
+
+#include "socket.hpp"
+
+class UDPSocket : public Socket {
+	public:
+		UDPSocket();
+		~UDPSocket();
+
+		bool bind(uint16_t port);
+		bool send_to(const std::string& host, uint16_t port, const std::string& data);
+		std::string receive_from(size_t max_length, std::string& host, uint16_t& port);
+};
+
+#endif
+
