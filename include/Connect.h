692523365efca10f035f8c533370469e8a4c0ba2
golfrumors <golfrumors@users.noreply.github.com>
2023-02-09 20:52:14 -0500

    fix lack of definition error

--- include/Connect.h
+++ include/Connect.h
@@ -1,6 +1,10 @@
 #ifndef CONNECT_H
 #define CONNECT_H
 
+#include <string>
+
+//funcs to handle network conns
+
 int createConnection(const std::string& ip, int port);
 void sendData(int socket, const std::string& data);
 std::string receiveData(int socket, uint32_t buffSize = 0);
