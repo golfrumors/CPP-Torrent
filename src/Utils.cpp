169cd1abcb13a12a3a65b68a025366cd653d6048
golfrumors <golfrumors@users.noreply.github.com>
2023-02-10 00:10:16 -0500

    create first commit, add Utils.cpp, implement fastMemcpy func

---
+++ src/Utils.cpp
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <stdint.h>
+
+//AVX intrinsic functions header
+#include <immintrin.h>
+
+#include "include/Utils.h"
+
+
+
+void fastMemcpy(void* ptrDest, void* ptrSrc, size_t nBytes) {
+        assert(nBytes % 32 == 0);
+        assert((intptr_t(ptrDest) & 31) == 0);
+        assert((intptr_t(ptrSrc) & 31) == 0);
+        const __m256i *pSrc = reinterpret_cast<const __m256i*>(ptrSrc);
+        __m256i *pDest = reinterpret_cast<__m256i*>(ptrDest);
+        int64_t nVects = nBytes / sizeof(*pSrc);
+        for (; nVects > 0; nVects--, pSrc++, pDest++) {
+            const __m256i loaded = _mm256_stream_load_si256(pSrc);
+            _mm256_stream_si256(pDest, loaded);
+        }
+        _mm_sfence();
+}
