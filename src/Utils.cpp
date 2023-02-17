4480ea7f10d81a46af796f00a512b045a6c8c8b6
golfrumors <30129070+golfrumors@users.noreply.github.com>
2023-02-17 04:43:29 -0500

    utils.cpp added more funcs

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,5 +1,10 @@
 #include <assert.h>
 #include <stdint.h>
+#include <iomanip>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <bitset>
 
 //AVX intrinsic functions header
 #include <immintrin.h>
@@ -20,3 +25,74 @@ void fastMemcpy(void* ptrDest, void* ptrSrc, size_t nBytes) {
         }
         _mm_sfence();
 }
+
+std::string urlEncode(const std::string& value) {
+	std::ostringstream escaped;
+	escaped.fill('0');
+	escaped << std::hex;
+	for(char c : value) {
+		if(isalnum(c) || == '-' || c == '_' || c == '.' || c == '~'){
+			escaped << c;
+			continue;
+		}
+
+		escaped << std::uppercase;
+		escaped << '%' << std::setw(2) << int((unsigned char) c);
+		escaped << std::nouppercase;
+	}
+
+	return escaped.str();
+}
+
+std::string hexDecode(const std::string& value) {
+	int hashLength = value.length();
+	std::string decoded;
+	for(int i = 0; i < hashLength; i += 2) {
+		std::string byte = value.substr(i, 2);
+		char chr = (char) (int)strtol(byte.c_str(), nullptr, 16);
+		decoded.push_back(chr);
+	}
+
+	return decoded;
+}
+
+std::string hexEncode(const std::string& value) {
+	static const char hexDig[] = "0123456789ABCDEF";
+
+	std::string encoded;
+	encoded.reserve(value.length() * 2);
+
+	for(unsigned char c : value) {
+		encoded.push_back('\\');
+		encoded.push_back('x');
+		encoded.push_back(hexDig[c >> 4]);
+		encoded.push_back(hexDig[c & 15]);
+	}
+
+	return encoded;
+}
+
+bool hasPiece(const std::string& bitfield, int pieceIndex) {
+	int byteIndex = floor(pieceIndex / 8);
+	int bitIndex = pieceIndex % 8;
+
+	return (bitfield[byteIndex] >> (7- bitIndex) & 1) != 0;
+}
+
+void setPiece(std::string& bitfield, int pieceIndex) {
+	int byteIndex = floor(pieceIndex / 8);
+	int bitIndex = pieceIndex % 8;
+
+	bitfield[byteIndex] |= 1 << (7 - bitIndex);
+}
+
+//bytesToInteger from string using bitwise operation to convert
+int bytesToInt(std::string bytes) {
+	int result = 0;
+	for (int i = 0; i < 4; i++) {
+		result = (result << 8) + (unsigned char)bytes[i];
+	}
+
+	return result;
+}
+
